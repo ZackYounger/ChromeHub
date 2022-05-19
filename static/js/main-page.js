48bdd30ffdfac9f3eed9be8edb0e7e38077fe4fa
ZackYounger <zack5@hotmail.com>
2022-05-19 23:25:10 +0100

    5 to 11:30 .....

--- static/js/main-page.js
+++ static/js/main-page.js
@@ -1,66 +1,88 @@
-let x = 0;
-let startGame;
-startGame = false;
-let intervalCount = 0;
-const buttonNames = ['play','profile','about','settings','x','y'];
+const root = document.querySelector(':root');
 
-var root = document.querySelector(':root');
+const sitesURLs = ['https://github.com/ZackYounger',
+                   'https://hero.highgateschool.org.uk/planner',
+                   'https://outlook.office.com/mail/',
+                   'https://www.twitch.tv/',
+                   'https://twitter.com/',
+                   'https://www.youtube.com/'];
+
+var closest;
+var distance;
+var pushDistance;
+var highlightDistance = 400;
+var selectionDistance = 80;
+var search;
 
 window.onload  = function () {
-    var window = document.getElementById("window");
-    var ul = document.getElementsByTagName('ul')[0];
-    var liList = document.getElementsByTagName('li');
-    var orbit = document.getElementById('orbit');
-    var dict = {};
+    const selectorMenu = document.getElementById('selectorMenu');
+    const selector = document.getElementById('selector');
+    const selectorData = selector.getBoundingClientRect();
+    const containers = document.getElementsByClassName('svg-container');
+    search = document.getElementsByClassName('searchbar2')[0];
 
-    //build dictionary
-    for (let i=0;i<6;i++) {
-        dict[buttonNames[i]] = liList[i];
+    selector.style.transition = '0ms';
+    root.style.setProperty('--distanceSelector', "0%");
+
+    var containersX = containers[0].getBoundingClientRect().x + containers[0].getBoundingClientRect().width/2 - selectorData.width/2;
+    var accContainersX = containersX + selectorData.width/2;
+    const containerHeight = containers[0].getBoundingClientRect().height;
+
+    const containerCoords = [];
+    for (container of containers) {
+        const data = container.getBoundingClientRect();
+        containerCoords.push(data.y + data.height - selectorData.height/2 - 10);
     }
-    console.log(dict);
 
-    //opening animation
-    for (let j=0;j<6;j++) {
-        setTimeout(function () {
-            liList[j].classList.toggle('pushObject-' + buttonNames[j]);
-        }, 100 * j);
+    function mousemove(event) {
+        selector.style.transition = '200ms';
+        //if (event != null) {
+            //console.log(
+            //    "clientX: ", event.clientX,
+            //    "clientY:", event.clientY);
+            //}
+
+        closest = containerCoords.reduce((a, b) => {
+            return Math.abs(b - event.clientY) < Math.abs(a - event.clientY) ? b : a;
+        });
+
+
+        root.style.setProperty('--selectorX', containersX.toString() + "px");
+        root.style.setProperty('--selectorY', (closest - containerHeight/2 + 10).toString() + "px");
+
+        distance = (Math.abs(event.clientX - accContainersX));
+        if (distance < highlightDistance) {
+            pushDistance = (highlightDistance - distance)/(highlightDistance/100);
+        } else {
+            pushDistance = 0;
+        }
+        root.style.setProperty('--distanceSelector', pushDistance.toString() + "%");
+
+        root.style.setProperty('--mouseX', event.clientX.toString() + "px");
+        root.style.setProperty('--mouseY', event.clientY.toString() + "px");
     }
 
+    window.addEventListener('mousemove', mousemove);
 
-    setInterval(function () {
+    function mouseclick(event) {
+        console.log(distance , selectionDistance/2)
+        if (distance < selectionDistance) {
+            const index = containerCoords.indexOf(closest);
+            const redirectLink = sitesURLs[index];
+            window.location.href = redirectLink;
+        }
+    }
 
-        dict['settings'].onclick = function (e) {
-            //dict['settings'].classList.toggle('pushObject-settings');
-            orbit.classList.toggle('redirect-orbit');
-            //pause rotation
-            const running = ul.style.animationPlayState === 'paused';
-            ul.style.animationPlayState = running ? 'running' : 'paused';
-            for (let j=0;j<6;j++) {
-                for (let i=0;i<2;i++) {
-                    liList[j].children[i].style.animationPlayState = running ? 'running' : 'paused';
-                }
-            }
-            var settingsCoords = dict['settings'].getBoundingClientRect();
-            var windowCoords = window.getBoundingClientRect();
-            const currentAngle = Math.atan((settingsCoords.top - windowCoords.top)/(settingsCoords.left - windowCoords.left));
-            console.log(currentAngle*(180/Math.PI));
-            root.style.setProperty('--windowTranslateX','250px');
-            root.style.setProperty('--windowRotate',(-currentAngle).toString()+'rad');
-            console.log(currentAngle.toString()+'rad');
-        };
+    window.addEventListener('mousedown',mouseclick);
 
-        //pause rotation
-        window.onclick = function () {
-            orbit.classList.toggle('redirect-orbit');
-            window.classList.toggle('redirect-window');
-            const running = ul.style.animationPlayState === 'paused';
-            ul.style.animationPlayState = running ? 'running' : 'paused';
-            for (let j=0;j<6;j++) {
-                for (let i=0;i<2;i++) {
-                    liList[j].children[i].style.animationPlayState = running ? 'running' : 'paused';
-                }
-            }
-        };
+    function enter(event) {
+        console.log('enter unction')
+        if (event.key === "Enter") {
+            console.log("enter")
+            redirectLink = "https://www.google.com/search?q=" + search.value.replaceAll(" ","+")
+            window.location.href = redirectLink;
+        }
+    }
 
-    },10);
+    window.addEventListener("keypress", enter);
 };
