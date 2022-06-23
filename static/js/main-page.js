8268da5fd1da8a3d6199a57e1636bc840bd50c55
Zack <zack5@hotmail.com>
2022-06-23 14:10:47 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- static/js/main-page.js
+++ static/js/main-page.js
@@ -102,10 +102,14 @@ function updateWeatherStats (stats) {
 window.onload  = function () {
 
     // Add squares
+
+    //var data = JSON.parse(calandar.csv);
+
     console.log('run')
     const squares = document.querySelector('.squares');
     for (var i = 1; i < 365; i++) {
-        const level = Math.floor(Math.random() * 3);squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+        const level = Math.floor(Math.random() * 3);
+        squares.insertAdjacentHTML('beforeend', `<li data-level="${level}" style="background: red;"></li>`);
     }
 
 
